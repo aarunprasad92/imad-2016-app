d92a086c1a17efdf1a2c0a9c521dd7b3456c5c76
aarunprasad92 <aarunprasad92@gmail.com>
2016-10-09 12:19:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 var mainElement = document.getElementById('main');
 mainElement.innerHTML = 'New Value';
 
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.syle.marginLeft = marginLeft + 'px';
+}
 var img = document.getElementById('madi');
 img.onclick = function(){
-    img.style.marginLeft = '500px';
+    setInterval(moveRight, 100);
+    //img.style.marginLeft = '500px';
 };
