010c15b2c895f2769d27857fe166d8d6312b912a
aarunprasad92 <aarunprasad92@gmail.com>
2016-10-09 13:51:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,27 @@
 console.log('Loaded!');
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    console.log('Image clicked..');
-    var interval = setInterval(moveRight, 50);
+var button = document.getElementById('click');
+
+button.onclick = function()
+{
+    //create a request
+    var request = new XMLHttpRequest();
+    
+    //make request
+    request.open('GET','http://aarunprasad92.imad.hasura-app.io/',true);
+    request.send(null);
+    
+    //capture the response and do something
+    request.onreadystatecjange = function()
+    {
+      if (request.readyState === XMLHttpRequest.DONE)
+      {
+          if (request.status === 200)
+          {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML= counter.toString();
+          }
+      }
+    };
 };
