659be1f24fa32b96f7eba8eea52073ea6f129b01
aarunprasad92 <aarunprasad92@gmail.com>
2016-10-09 12:03:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var mainElement = document.getElementById('main');
 mainElement.innerHTML = 'New Value';
+
+var img = document.getElementById('madi');
+img.onClick = function(){
+    img.style.leftMargin = '100px';
+}
