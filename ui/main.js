aa1aa392a117cdab0f71ba55da97757a8532bfac
aarunprasad92 <aarunprasad92@gmail.com>
2016-10-09 17:37:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,5 +34,31 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = function()
 {
+    //create a request
+    var request = new XMLHttpRequest();
+
+    //capture the response and do something
+    request.onreadystatechange = function()
+    {
+      if (request.readyState === XMLHttpRequest.DONE)
+      {
+          if (request.status === 200)
+          {
+              var names = request.responseText;
+              names = JSON.parse(names);
+              
+              var list = '';
+              for (var i=0; i<names.length; i++)
+              {
+                  list = '<li>' + names[i] + '</li>'
+              }
+              var ul = document.getElementById('nameList');
+              ul.innerHTML= list;
+          }
+      }
+    };
     
+     //make request
+    request.open('GET','http://aarunprasad92.imad.hasura-app.io/submit-name?name='+nameVal,true);
+    request.send(null);
 };
