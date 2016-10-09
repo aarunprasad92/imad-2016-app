b46a68bacd65cf8c00e6cc89a95e93e7dbb765e7
aarunprasad92 <aarunprasad92@gmail.com>
2016-10-09 17:30:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,14 @@ button.onclick = function()
     request.open('GET','http://aarunprasad92.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var nameVal = nameInput.value;
+
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function()
+{
+    
+};
